ed0ecced86a063868c4db88fa59d2b2e8e3081c6
tif-calin <culitif@tuta.io>
2021-04-27 19:50:07 -0700

    implement  functions

--- src/array-sort.js
+++ src/array-sort.js
@@ -1,20 +1,20 @@
 export function leastToGreatest(arr){
-  
+  return arr.sort((a, b) => a - b);
 }
 
 export function greatestToLeast(arr){
-  
+  return arr.sort((a, b) => b - a);
 }
 
 export function lengthSort(arr){
-  
+  return arr.sort((a, b) => a.length - b.length);
 }
 
 export function alphabetical(arr){
-  
+  return arr.sort();
 }
 
 export function byAge(arr){
-  
+  return arr.sort((a, b) => a.age - b.age);
 }
 
